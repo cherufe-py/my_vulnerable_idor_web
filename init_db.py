c75a87c9537ba114be78a953ebec7c2cae0366bd
cherufe.py <cherufe.py@gmail.com>
2025-10-16 16:43:35 -0300

    lets fooo

--- init_db.py
+++ init_db.py
@@ -3,14 +3,12 @@ import sqlite3
 
 APP_DB = 'example.db'
 
-# Remover DB si existe (para repetir el flujo fácilmente)
 if os.path.exists(APP_DB):
     os.remove(APP_DB)
 
 db = sqlite3.connect(APP_DB)
 cur = db.cursor()
 
-# tabla users mínima (puedes añadir hashing si quieres)
 cur.execute('''
     CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -19,7 +17,6 @@ cur.execute('''
     )
 ''')
 
-# tabla files ahora guarda nombre a mostrar, nombre físico en disco y content_type
 cur.execute('''
     CREATE TABLE files (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -31,7 +28,6 @@ cur.execute('''
     )
 ''')
 
-# Seed usuarios (sin archivos)
 cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('alice', 'alicepass'))
 cur.execute('INSERT INTO users (username, password) VALUES (?, ?)', ('bob', 'bobpass'))
 
